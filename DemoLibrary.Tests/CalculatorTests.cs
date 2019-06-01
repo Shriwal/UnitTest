aa08d55c421c963fe5b51ae30c590da7b115bab0
Ankit Shriwal <a.shriwal@ingress-electronics.com>
2019-06-01 20:46:52 +0200

    WIP: Addition of One Test Case.

---
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        public void Add_SimpleValuesShouldCalculate()
+        {
+            double expected = 5;
+
+            double actual = Calculator.Add(3, 5);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
