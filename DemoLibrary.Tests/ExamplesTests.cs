f7292e856307a08953e9ce6fcc5ca1a043e3c28b
Ankit Shriwal <a.shriwal@ingress-electronics.com>
2019-06-01 23:14:29 +0200

    WIP: Add of test case ExampleLoadTextFile_ValidNameShouldWork()

--- DemoLibrary.Tests/ExamplesTests.cs
+++ DemoLibrary.Tests/ExamplesTests.cs
@@ -11,6 +11,12 @@ namespace DemoLibrary.Tests
 {
     public class ExamplesTests
     {
-     
+        [Fact]
+        public void ExampleLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = Examples.ExampleLoadTextFile("This is a valid file name");
+
+            Assert.True(actual.Length > 0);
+        }
     }
 }
